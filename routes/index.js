70e3d68dd1ef9e5eb0bdb072c90473129c591a90
Adeniji Opeyemi <adenijiopeyemi68@gmail.com>
2022-08-15 21:57:46 +0100

    made changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Yemi and Okiri First nodejs deployment using azure web app' });
 });
 
 module.exports = router;
